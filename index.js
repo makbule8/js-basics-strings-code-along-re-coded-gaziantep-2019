cb029d5064aca928798fb099dc8d65fcb0524b80
makbule özbay <makbuleozbay8@gmail.com>
2019-08-02 23:01:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Write your code in this file!
 
 const currentUser = "Billy Bauer";
-
 const welcomeMessage = `Welcome to Flatbook ${currentUser}!`;
-
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-
 const shortGreeting = `Welcome,${currentUser.slice(0,1)}!`;
